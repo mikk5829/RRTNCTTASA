1bc0e3b446cd4eb339b7aa7f872cdd1f6347521d
Mikkel <s184230@student.dtu.dk>
2023-09-12 10:44:07 +0200

    feat: started the python structure

---
+++ python-version/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('../LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='RRTNCTTASA',
+    version='0.0.1',
+    description='Robust Real-Time Non-Cooperative Target Tracking Algorithm for Space Applications',
+    long_description=readme,
+    author='Mikkel Anderson & Jonas Nielsen',
+    author_email='s184230@dtu.dk',
+    url='https://github.com/mikk5829/RRTNCTTASA',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
